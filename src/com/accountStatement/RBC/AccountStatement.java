0d6f873a3eb1067a6d76c3827ef986ddcc91376f
unknown <raghavendra064@gmail.com>
2019-06-29 23:58:04 +0530

    Updating project

---
+++ src/com/accountStatement/RBC/AccountStatement.java
@@ -0,0 +1,11 @@
+package com.accountStatement.RBC;
+
+public class AccountStatement {
+	public static void selectDateRange() {
+		
+	}
+	
+	public static void selectAccountNumber() {
+		
+	}
+}
---
+++ src/com/transferAmount/RBC/InterAC.java
@@ -0,0 +1,12 @@
+package com.transferAmount.RBC;
+
+public class InterAC {
+	public static void enterAmount() {
+		
+	}
+	
+	public static void selectRecepient() {
+		
+	}
+	
+}

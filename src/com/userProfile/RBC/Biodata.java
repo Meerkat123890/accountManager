13b8677e71b840692de5683e59c243f398e8bd15
unknown <raghavendra064@gmail.com>
2019-07-13 19:25:02 -0400

    Adding a getter for BIO

--- src/com/userProfile/RBC/Biodata.java
+++ src/com/userProfile/RBC/Biodata.java
@@ -3,11 +3,16 @@ package com.userProfile.RBC;
 public class Biodata {
 	private String name, nationality, sex, SIN;
 	private int age;
-	private Biodata(String name, String nationality, String sex, String SIN, int age) {
+	private Biodata(String name, String nationality, String sex, int age) {
 		this.name = name;
 		this.nationality = nationality;
 		this.sex = sex;
 		this.SIN = SIN;
 		this.age = age;
+	}
+	
+	public Object getBio() {
+		return String.format("Name:", "%8s", name, "/n", "Sex:", "%8s", sex, "/n", "SIN:", "%8s", SIN, "/n", "Age:","%8s", age);
+		 
 	}
 }
